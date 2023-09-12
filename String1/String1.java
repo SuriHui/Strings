a7eee2a6446d08e0295531d10a14cc1efac435c8
SuriHui <143148869+SuriHui@users.noreply.github.com>
2023-09-12 11:22:32 -0700

    Add files via upload

---
+++ String1/String1.java
@@ -0,0 +1,16 @@
+public class String1
+{
+    public static void main(String[] args)
+    {
+        String str1 = "I am a string";
+        String str2 = str1;
+        String str3 = "I am also a string";
+        boolean stringsEqual;
+        if (str1 == str3) {
+            stringsEqual = true;
+        } else {
+            stringsEqual = false;
+        }
+        
+     }
+}
